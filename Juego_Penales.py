aeda61c31efe0b7ebd4219cc8637fa292597c3ac
Kimvo1 <carvaduarte@hotmail.com>
2023-09-22 18:13:44 -0500

    cambio poo

--- Juego_Penales.py
+++ Juego_Penales.py
@@ -1,20 +1,38 @@
-while True:
-    print("Bienvenido al juego de atajar y cobrar penaltis")
-    nombre = input("Cual es tu nombre?: ")
-    print(nombre,",", '''Seleccione una opción:
-    1. Atajar
-    2. Cobrar
-    para salir oprima: s''')
+class JuegoPenaltis:
+    def __init__(self):
+        print("!Bienvenido al juego de atajar y cobrar penaltis¡")
+        self.nombre_usuario = input("¿Cuál es tu nombre?: ")
 
-    opcion = input("Ingrese el número de la opción que desea seleccionar: ")
-    if opcion == "1":
-        print(''' ¿Hacia dónde quieres atajar? 
-        1. Izquierda
-        2. Centro
-        3. Derecha
-        para salir oprima: S'''
-        )
+    def menu_principal(self):
+        print(self.nombre_usuario,","
+            "Seleccione una opción:\n"
+            "1. Atajar\n"
+            "2. Cobrar\n"
+            "Para salir, oprima: s")
+
+    def iniciar_juego(self):
+        while True:
+            self.menu_principal()
+            opcion = input("Ingrese el número de la opción que desea seleccionar: ")
+            if opcion == "1":
+                self.atajar()
+            elif opcion == "2":
+                self.cobrar()
+            elif opcion == "s":
+                print("¡Gracias por jugar!")
+                break
+            else:
+                print("Opción no válida. Por favor, seleccione una opción válida.")
+
+    def atajar(self):
+        print("¿Hacia dónde quieres atajar?\n"
+            "1. Izquierda\n"
+            "2. Centro\n"
+            "3. Derecha\n"
+            "Para salir, oprima: s")
+
         opcion_atajar = input("Ingrese el número de la opción que desea seleccionar: ")
+
         if opcion_atajar == "1":
             print("¡Atajaste hacia la izquierda!, Ganaste!")
         elif opcion_atajar == "2":
@@ -23,27 +41,33 @@ while True:
             print("¡Atajaste hacia la derecha!, Ganaste")
         elif opcion_atajar == "s":
             print("¡Gracias por jugar!")
-            break
-        
         else:
             print("Opción no válida. Por favor, seleccione una opción válida.")
-    elif opcion == "2":
-        print('''¿Hacia dónde quieres cobrar?
-        1. Izquierda
-        2. Centro
-        3. Derecha''')
-        opcion_cobrar = input("ingrese el numero de la opcion que desea seleccionar: ")
+
+    def cobrar(self):
+        print("¿Hacia dónde quieres cobrar?\n"
+            "1. Izquierda\n"
+            "2. Centro\n"
+            "3. Derecha")
+
+        opcion_cobrar = input("Ingrese el número de la opción que desea seleccionar: ")
+        potencia = input("seleciona la potencia del disparo:\n"
+                        "1. Suave\n"
+                        "2. Medio\n"
+                        "3. Fuerte\n"
+                        "escribe la opcion: ")
+
         if opcion_cobrar == "1":
-            print("¡Cobraste hacia la izquierda!, ¡Se estrello en el travesaño!, Perdiste! :()")
+            print(potencia,"," "¡Cobraste hacia la izquierda!, ¡Se estrelló en el travesaño!, Perdiste! :(")
         elif opcion_cobrar == "2":
-            print("¡Cobraste hacia el centro!, Atajado, Perdiste! :()")
+            print(potencia,"," "¡Cobraste hacia el centro!, Atajado, Perdiste! :(")
         elif opcion_cobrar == "3":
-            print("¡Cobraste hacia la derecha!, Goool!, Ganaste")
-    elif opcion == "s":
-        print("¡Gracias por jugar!")
-        break
-    else:
-        print("Opción no válida. Por favor, seleccione una opción válida.")
-    
-        
+            print(potencia,"," "¡Cobraste hacia la derecha!, Goool!, Ganaste")
+        else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+
+
+if __name__ == "__main__":
+    juego = JuegoPenaltis()
+    juego.iniciar_juego()
 
